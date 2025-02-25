ebcd24b4b244ea863e7dd25913a51888e545b9d4
TamirhaG <96215456+TamirhaG@users.noreply.github.com>
2025-02-24 20:58:45 -0500

    cambios

--- logger.py
+++ logger.py
@@ -1,19 +1,19 @@
 import logging
-import os
+import streamlit as st
 
-def setup_logger(name, log_file, level=logging.INFO):
-    os.makedirs(os.path.dirname(log_file), exist_ok=True)
+@st.cache_resource
+def setup_logger(name, level=logging.INFO):
     logger = logging.getLogger(name)
     logger.setLevel(level)
 
     if not logger.handlers:
-        handler = logging.FileHandler(log_file)
+        handler = logging.StreamHandler()
         handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
         logger.addHandler(handler)
 
     return logger
 
 # Creación de los loggers
-activity_logger = setup_logger("activity", "logs/activity.log", logging.INFO)
-warning_logger = setup_logger("warning", "logs/warning.log", logging.WARNING)
-error_logger = setup_logger("error", "logs/error.log", logging.ERROR)
+activity_logger = setup_logger("activity", logging.INFO)
+warning_logger = setup_logger("warning", logging.WARNING)
+error_logger = setup_logger("error", logging.ERROR)
